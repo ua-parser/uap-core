b3a6e3e49b7be8da231fef12dd325d34ddf03633
commenthol <commenthol@gmail.com>
2014-12-04 21:39:10 +0100

    test_device_brandmodel.yaml merged into test_device.yaml

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,5 @@
 var assert = require('assert'),
-    path = require('path'), 
+    path = require('path'),
     fs = require('fs'),
     yaml = require('yamlparser'),
 	refImpl = require('uap-ref-impl')(readYAML('../regexes.yaml'));
@@ -48,7 +48,7 @@ function msg(name, actual, expected) {
   });
 });
 
-['../tests/test_device.yaml', '../tests/test_device_brandmodel.yaml'].forEach(function(fileName) {
+['../tests/test_device.yaml'].forEach(function(fileName) {
   var fixtures = readYAML(fileName).test_cases;
   suite(fileName, function() {
     fixtures.forEach(function(f) {
